29d4bd83163b2b9460ea96ff54ee97fe43d2b50e
Oleksandr Stepanenko <sashastepanenko2007@gmail.com>
2021-05-06 14:37:02 +0300

    delete comments

--- src/transform.js
+++ src/transform.js
@@ -1,11 +1,5 @@
-// assign 80 once you accept a challenge to implement Level 80 requirements
 export const level = 80;
-/*
-  Add parameters as appropriate.
-  Should allow arbitrary number of expressions.
-  Should work with arbitrary function expressions passed.
-  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
-*/
+
 export function transform(stringsArg, ...keysArg) {
   const strings = [...stringsArg];
   const keys = [...keysArg];
@@ -32,16 +26,7 @@ export function transform(stringsArg, ...keysArg) {
     strings[0],
   );
 }
-/*
-  Level 1: expects no parameters. Returns its own name capitalized.
-    Usage example:
-      testFunction() should return 'TESTFUNCTION'
-  Level 80: expects 2 parameters. Returns its own name capitalized and both parameters' values appended to it.
-    Whitespaces should be trimmed from the first parameter.
-    Parameters values are glued with '=' and parenthesized.
-    Usage example:
-      testFunction("some Text", 125) should return 'TESTFUNCTION(someText=125)'
- */
+
 export function testFunction(string, expression) {
   const name = testFunction.name.toUpperCase();
   string = string.replace(/\s/g, '');
